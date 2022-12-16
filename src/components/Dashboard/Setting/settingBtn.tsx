05e6dd2b15c73a42ea2e1d90a4be8b8dd7bad509
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2022-12-16 11:19:43 +0700

    Fix: remove Tooltips

--- src/components/Dashboard/Setting/settingBtn.tsx
+++ src/components/Dashboard/Setting/settingBtn.tsx
@@ -1,7 +1,7 @@
 
 import { motion } from 'framer-motion'
 import { IconType } from 'react-icons'
-import Tooltip from '@baseComponents/Tooltip';
+// import Tooltip from '@baseComponents/Tooltip';
 
 interface ControlBtnProps {
     Icon: IconType
